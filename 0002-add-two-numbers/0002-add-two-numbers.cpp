a18a2dd9c305d32444b8f67da1aa4596e54fd474
Rajeev Kumar <134843702+rkrustom@users.noreply.github.com>
2024-08-15 11:56:35 +0530

    Time: 27 ms (31.52%), Space: 77.4 MB (43.68%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+// ListNode* node = new ListNode(5);
+// Breakdown of the Code
+// new ListNode(5);: This line creates a new node using the new keyword. It calls the ListNode constructor with the value 5. The constructor initializes the node’s val to 5 and sets next to NULL.
+// ListNode* node = ...: The new keyword returns a pointer to the newly created node. This pointer is stored in the variable node.
+      ListNode *dummy = new ListNode(); 
+        ListNode *temp = dummy; 
+        int carry = 0;
+        while( (l1 != NULL || l2 != NULL) || carry) {
+            int sum = 0; 
+            if(l1 != NULL) {
+                sum += l1->val; 
+                l1 = l1 -> next; 
+            }
+            
+            if(l2 != NULL) {
+                sum += l2 -> val; 
+                l2 = l2 -> next; 
+            }
+            
+            sum += carry; 
+            carry = sum / 10; 
+            ListNode *node = new ListNode(sum % 10); 
+            temp -> next = node; 
+            temp = temp -> next; 
+        }
+        return dummy -> next; 
+    }
+};
