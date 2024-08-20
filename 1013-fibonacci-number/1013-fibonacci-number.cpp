65bdcf9e99ac13a8ea4d22e3605875b5857b4236
Rajeev Kumar <134843702+rkrustom@users.noreply.github.com>
2024-08-20 18:19:39 +0530

    Time: 0 ms (100%), Space: 7.6 MB (9.02%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
 int fib(int n,vector<int>&dp){
     if(n<=1) return n;
     //step--->3 memozation
@@ -8,12 +9,26 @@ int fib(int n,vector<int>&dp){
     dp[n]=fib(n-1,dp)+fib(n-2,dp);
     return dp[n];
 
-}
+}*/
     int fib(int n) {        
+   /*
    //step--->1 create a table to store the value of subproblem
     vector<int>dp(n+1,-1);
     int ans=fib(n,dp);
     return ans;
+    */
+//BOTTOM UP ---> TABULATION APPROACH
+    // Handle the base cases explicitly
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    vector<int>dp(n+1);
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
 
     }
 };
