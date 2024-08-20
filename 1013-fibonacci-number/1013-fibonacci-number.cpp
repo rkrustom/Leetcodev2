c9f02ab7ed95b92c3a6239e0611fcac3f3b4c334
Rajeev Kumar <134843702+rkrustom@users.noreply.github.com>
2024-08-20 17:41:30 +0530

    Time: 0 ms (100%), Space: 7.6 MB (9.02%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
 int fib(int n,vector<int>&dp){
     if(n<=1) return n;
+    //step--->3 memozation
     if(dp[n]!=-1) return dp[n];
-
+    //step--->2 first store the value then return 
     dp[n]=fib(n-1,dp)+fib(n-2,dp);
     return dp[n];
 
 }
     int fib(int n) {        
-    // if( n==0) return 0;
-    // if(n==1) return 1;
-    // else return fib(n-1)+fib(n-2);
+   //step--->1 create a table to store the value of subproblem
     vector<int>dp(n+1,-1);
     int ans=fib(n,dp);
     return ans;
