5a2b4f27b773e297d5f6bc3a96e05ec65d6be67a
Rajeev Kumar <134843702+rkrustom@users.noreply.github.com>
2024-09-10 11:47:51 +0530

    Time: 27 ms (33.6%), Space: 77.6 MB (44.76%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,32 +11,25 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+      int carry=0;
+      ListNode*dummy=new ListNode(0);
+        ListNode*temp=dummy;
+      while(l1!=NULL || l2!=NULL ||carry){
+         int sum=0;
+         if(l1!=NULL){
+            sum+=l1->val;
+            l1=l1->next;
+         }
+         if(l2!=NULL){
+            sum+=l2->val;
+            l2=l2->next;
+         }
+         sum+=carry;
+         temp->next=new ListNode(sum%10);
+         temp=temp->next;
+         carry=sum/10;
+      } 
+      return dummy->next;
 
-// ListNode* node = new ListNode(5);
-// Breakdown of the Code
-// new ListNode(5);: This line creates a new node using the new keyword. It calls the ListNode constructor with the value 5. The constructor initializes the node’s val to 5 and sets next to NULL.
-// ListNode* node = ...: The new keyword returns a pointer to the newly created node. This pointer is stored in the variable node.
-      ListNode *dummy = new ListNode(); 
-        ListNode *temp = dummy; 
-        int carry = 0;
-        while( (l1 != NULL || l2 != NULL) || carry) {
-            int sum = 0; 
-            if(l1 != NULL) {
-                sum += l1->val; 
-                l1 = l1 -> next; 
-            }
-            
-            if(l2 != NULL) {
-                sum += l2 -> val; 
-                l2 = l2 -> next; 
-            }
-            
-            sum += carry; 
-            carry = sum / 10; 
-            ListNode *node = new ListNode(sum % 10); 
-            temp -> next = node; 
-            temp = temp -> next; 
-        }
-        return dummy -> next; 
     }
 };
