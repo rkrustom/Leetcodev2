bf7c5ff3933b3763c4274d3416672ce8f4f87df2
Rajeev Kumar <134843702+rkrustom@users.noreply.github.com>
2024-10-15 17:54:18 +0530

    Time: 31 ms (73.47%), Space: 15.7 MB (45.68%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        long long swap = 0;
-        int black = 0; 
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '0') 
-                swap += (long long) black; 
-            else
-                black++; 
+        // long long swap = 0;
+        // int black = 0; 
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (s[i] == '0') 
+        //         swap += (long long) black; 
+        //     else
+        //         black++; 
+        // }
+        // return swap;
+        long long ans=0,c=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1') c++;
+            else ans+=c;
         }
-        return swap;
+        return ans;
     }
 };
