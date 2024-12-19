0fa7553fd71f3ffdcedb81335639a18be99026c8
Rajeev Kumar <134843702+rkrustom@users.noreply.github.com>
2024-12-19 22:54:27 +0530

    Time: 0 ms (100%), Space: 9.1 MB (35.22%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-    int n = arr.size();
-    if(n == 0) return 0;
-    vector<int> sortedArr = arr;
-    sort(sortedArr.begin(), sortedArr.end());
-    
-    int maxChunks = 1; 
-    int totalSplits = 1 << (n - 1); 
-
-    for(int mask = 0; mask < totalSplits; ++mask){
-       vector<std::vector<int>> chunks;
-        vector<int> currentChunk;
-        for(int i = 0; i < n; ++i){
-            currentChunk.push_back(arr[i]);
-            if(i < n -1 && (mask & (1 << i))){
-                chunks.push_back(currentChunk);
-                currentChunk.clear();
-            }
+    int max_so_far = -1;
+    int chunks = 0;   
+    for(int i = 0; i < arr.size(); ++i){
+        max_so_far = std::max(max_so_far, arr[i]);
+        if(max_so_far == i){
+        chunks++;
         }
-        if(!currentChunk.empty()){
-            chunks.push_back(currentChunk);
-        }
-        vector<int> concatenated;
-        for(auto &chunk : chunks){
-            vector<int> sortedChunk = chunk;
-            sort(sortedChunk.begin(), sortedChunk.end());
-            concatenated.insert(concatenated.end(), sortedChunk.begin(), sortedChunk.end());
-        }
-        if(concatenated == sortedArr){
-            maxChunks = std::max(maxChunks, (int)chunks.size());
-        }
-    }
-
-    return maxChunks;  
+    }   
+    return chunks;  
     }
 };
