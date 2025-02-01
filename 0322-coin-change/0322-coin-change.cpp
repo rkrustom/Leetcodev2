9ecbcf201bdfbb47358d623cc19ff01b6275bd3f
Rajeev Kumar <134843702+rkrustom@users.noreply.github.com>
2025-02-01 10:18:15 +0530

    Time: 62 ms (14.01%), Space: 53.9 MB (10.39%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,54 +1,33 @@
-class Solution{
-	public:
-	int solveRec(vector<int>&nums,int amount){
-	    //base case
-	    if(amount==0) return 0;
-	    if(amount<0) return INT_MAX;
-	    int mini=INT_MAX;
-	    for(int i=0;i<nums.size();i++){
-	        int ans=solveRec(nums,amount-nums[i]);
-	        if(ans!=INT_MAX)
-	        mini=min(mini,1+ans);
-	        
-	    }
-	    return mini;
-	    
-	}
-	
-		int solvemem(vector<int>&nums,int amount,vector<int>&dp){
-	    //base case
-	    if(amount==0) return 0;
-	    if(amount<0) return INT_MAX;
-	    
-	    //step---->3
-	   if(dp[amount]!=-1) return dp[amount];
+class Solution {
+public:
+    int solve(int i, int amount, vector<int>&coins,vector<vector<int>>&dp){
+        int n=coins.size();
+        //base case
+        if(amount==0){
+            return 0;
+        }
+        if(i>=n ){
+            return 1e9;
+        }
+        // if(amount<coins[i]) return 1e9;
+        if(dp[i][amount]!=-1){
+            return dp[i][amount];
+        }
+      int inc=1e9;
+      int exc;
+      if(amount>=coins[i])
+      inc=1+solve(i,amount-coins[i],coins,dp);
+      exc=solve(i+1,amount,coins,dp);
+      return dp[i][amount]=min(inc,exc);
+      
 
-	    
-	    int mini=INT_MAX;
-	    for(int i=0;i<nums.size();i++){
-	        int ans=solvemem(nums,amount-nums[i],dp);
-	        if(ans!=INT_MAX)
-	        mini=min(mini,1+ans);
-	        
-	    }
-	    dp[amount]=mini;
-	    return mini;
-	    
-	}
-	
-	
-	
-	int coinChange(vector<int>nums, int amount)
-	{
-	   // int ans=solveRec(nums,amount);
-	   // if(ans==INT_MAX) return -1;
-	   // return ans;
-	   
-	   int n=nums.size();
-	   //step---->1
-	   vector<int> dp(amount+1,-1);
-	   int ans=solvemem(nums,amount,dp);
-	   if(ans==INT_MAX) return -1;
-	   return ans;
-	}
+
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int i=0;
+        int n=coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        int ans=solve(i,amount,coins,dp);
+        return (ans >= 1e9) ? -1 : ans;
+    }
 };
