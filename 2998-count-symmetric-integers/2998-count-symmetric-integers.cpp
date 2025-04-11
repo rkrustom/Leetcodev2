65834b0f3ed68812c392339548fe077012bc603a
Rajeev Kumar <134843702+rkrustom@users.noreply.github.com>
2025-04-11 09:51:18 +0530

    Time: 67 ms (63.62%), Space: 9.8 MB (56.7%) - LeetHub

---
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+       int count = 0;
+        for (int num = low; num <= high; ++num) {
+            string s = to_string(num);
+            int n = s.length();
+            if (n % 2 != 0) continue; 
+            
+            int half = n / 2;
+            int sum1 = 0, sum2 = 0;
+            for (int i = 0; i < half; ++i) {
+                sum1 += s[i] - '0'; 
+            }
+            for (int i = half; i < n; ++i) {
+                sum2 += s[i] - '0'; // sum of second half digits
+            }
+            if (sum1 == sum2) {
+                count++;
+            }
+        }
+        return count; 
+    }
+};
